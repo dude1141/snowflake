3552df859c9489a5e2c706cdb3de3d40953eb8e7
Mithlesh_1141 <48873398+dude1141@users.noreply.github.com>
2025-02-02 20:21:03 -0500

    Create user_access_and_roles.sql

---
+++ user_access_and_roles.sql
@@ -0,0 +1,4 @@
+once you provide access to roles ,the users assinged to role automatically gets acess to db 
+and tables if role has access or usage on database and tables
+
+grant usage on database testdb TO ROLE region_east
