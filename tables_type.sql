74827a8696234810df5cd26f5af2d2adfd2298d7
Mithlesh_1141 <48873398+dude1141@users.noreply.github.com>
2025-02-04 20:16:21 -0500

    Create tables_type.sql

---
+++ tables_type.sql
@@ -0,0 +1,28 @@
+Permanenet table
+
+standard table that persists data permanently untill
+explicity dropped.
+is failsafe.
+
+
+Transient---does not support fail safe-recovery
+has time travel limited to day1
+
+temporary staging tables use case
+its physcial table
+
+
+temporary table.
+used in stored proceedure temporarily.
+
+
+External:
+ stores data in to awss3 or hdfs
+ snowflake des not have CONTROL
+ 
+
+Materialised view:
+
+--
+
+
